2d1ff046b83a20016eb78d735140f8077e072479
Smeunzo <fsl12@hotmail.fr>
2020-12-23 15:56:33 +0100

    test

--- services/customer/src/main/java/com/emazon/services/customer/CustomerApplication.java
+++ services/customer/src/main/java/com/emazon/services/customer/CustomerApplication.java
@@ -1,7 +1,12 @@
 package com.emazon.services.customer;
 
+import com.emazon.services.customer.dao.CustomerRepository;
+import com.emazon.services.customer.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerApplication {
@@ -10,4 +15,14 @@ public class CustomerApplication {
 		SpringApplication.run(CustomerApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner start(CustomerRepository cr){
+		return args -> {
+			Customer c = new Customer();
+			c.setFirstName("Abdelhakim");
+			c.setLastName("BELHACHEMI");
+
+			cr.save(c);
+		};
+	}
 }
---
+++ services/customer/src/main/java/com/emazon/services/customer/dao/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.emazon.services.customer.dao;
+
+import com.emazon.services.customer.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
