68a33572dda98171b3460455c13e70f3d3550ebc
belHak <hak721@outlook.fr>
2020-12-29 20:58:03 +0100

    user-service security

--- services/user/src/main/java/com/emazon/services/user/security/SecurityConfig.java
+++ services/user/src/main/java/com/emazon/services/user/security/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable(); // enable stateless authentication
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.headers().frameOptions().disable();//inactive frame security
+        http.headers().frameOptions().disable();//enable frame security
         http.authorizeRequests().antMatchers("/h2-console/**","/registerUser/**").permitAll();//enable access to h2 without authentication
 //        http.authorizeRequests().antMatchers(HttpMethod.POST,"/users/**").hasAnyAuthority("ADMIN"); Replaced by annotation in application
 //        http.authorizeRequests().antMatchers(HttpMethod.GET,"/users/**").hasAnyAuthority("USER");
