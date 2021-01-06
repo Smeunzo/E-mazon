0c47e1e01dda28e4f895367bb7a574ef86d105a6
belHak <hak721@outlook.fr>
2021-01-06 21:52:12 +0100

    commande line runner

--- services/inventory/src/main/java/com/emazon/services/inventory/InventoryApplication.java
+++ services/inventory/src/main/java/com/emazon/services/inventory/InventoryApplication.java
@@ -10,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 @EnableGlobalMethodSecurity(prePostEnabled = true,securedEnabled = true)// authorise prepost annotation
 public class InventoryApplication {
@@ -22,9 +24,10 @@ public class InventoryApplication {
 	CommandLineRunner start(ProductService productService, CategoryService categoryService){
 		return args -> {
 
-			Category phones = new Category();
-			phones.setName("phones");
+			Category phones = new Category(null,"phones",new ArrayList<>(),"catégory des telephones");
+			Category accessoires = new Category(null,"accessoires",new ArrayList<>(),"accessoires pour telephones");
 			categoryService.addCategory(phones);
+			categoryService.addCategory(accessoires);
 
 			Product product1 = new Product(null,100,"iphone",100,"description");
 			Product product2 = new Product(null,100,"samsung",100,"description");
@@ -34,6 +37,9 @@ public class InventoryApplication {
 			Product product6 = new Product(null,100,"sony",100,"description");
 			Product product7 = new Product(null,100,"oneplus",100,"description");
 
+			Product charger = new Product(null,100,"chargeur",50,"chargeur pour telephone");
+			Product memoire = new Product(null,100,"carte mémoire",50,"carte micro sd");
+
 			productService.addNewProduct(product1);
 			productService.addNewProduct(product2);
 			productService.addNewProduct(product3);
@@ -41,8 +47,11 @@ public class InventoryApplication {
 			productService.addNewProduct(product5);
 			productService.addNewProduct(product6);
 			productService.addNewProduct(product7);
+			productService.addNewProduct(charger);
+			productService.addNewProduct(memoire);
 
 			categoryService.linkProductsToCategory(phones,product1,product2,product3,product4,product5,product6,product7);
+			categoryService.linkProductsToCategory(accessoires,charger,memoire);
 
 		};
 	}
